b9118038a782a5fd6dd28aa2c4a623f41103bc72
sbailleul <44065690+sbailleul@users.noreply.github.com>
2023-12-21 14:07:56 +0000

    feat(challenge_6): End step 2

--- src/challenge_6.rs
+++ src/challenge_6.rs
@@ -1,8 +1,12 @@
-use std::fmt::{write, Display};
-#[derive(Debug)]
+use std::{
+    borrow::Borrow,
+    fmt::{write, Display},
+    iter,
+};
+#[derive(Debug, Clone)]
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 impl Display for Race {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -10,8 +14,30 @@ impl Display for Race {
     }
 }
 
+impl<'a> iter::Sum<&'a Self> for Race {
+    fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(
+            Race {
+                distance: 0,
+                time: 0,
+            },
+            |race1, race2| {
+                let res = Race {
+                    time: (race1.time.to_string() + &race2.time.to_string())
+                        .parse()
+                        .unwrap(),
+                    distance: (race1.distance.to_string() + &race2.distance.to_string())
+                        .parse()
+                        .unwrap(),
+                };
+                res
+            },
+        )
+    }
+}
+
 impl Race {
-    fn compute_ways_to_win_count(&self) -> u32 {
+    fn compute_ways_to_win_count(&self) -> u64 {
         let mut ways_to_win = 0;
         for i in 1..self.time - 1 {
             let distance = Self::get_distance_by_time(&i, &self.time);
@@ -22,7 +48,7 @@ impl Race {
         ways_to_win
     }
 
-    fn get_distance_by_time(hold_time: &u32, max_time: &u32) -> u32 {
+    fn get_distance_by_time(hold_time: &u64, max_time: &u64) -> u64 {
         hold_time * (max_time - hold_time)
     }
 }
@@ -33,7 +59,7 @@ impl From<&str> for LeaderBoard {
         let lines = value
             .lines()
             .map(|l| get_line_values(l))
-            .collect::<Vec<Vec<u32>>>();
+            .collect::<Vec<Vec<u64>>>();
         Self(
             lines[0]
                 .iter()
@@ -45,20 +71,23 @@ impl From<&str> for LeaderBoard {
 }
 
 impl LeaderBoard {
-    fn get_total_ways_to_win(&self) -> u32 {
+    fn get_total_ways_to_win(&self) -> u64 {
         self.0
             .iter()
             .map(|r| r.compute_ways_to_win_count())
             .reduce(|c1, c2| c1 * c2)
             .unwrap()
     }
+    fn get_total_ways_to_win_for_aggregated_races(&self) -> u64 {
+        self.0.iter().sum::<Race>().compute_ways_to_win_count()
+    }
 }
 
-fn get_line_values(line: &str) -> Vec<u32> {
+fn get_line_values(line: &str) -> Vec<u64> {
     let (_, values) = line.split_once(':').unwrap();
     values
         .split(' ')
-        .map(|v| v.parse::<u32>())
+        .map(|v| v.parse::<u64>())
         .filter_map(|r| r.ok())
         .collect()
 }
@@ -68,12 +97,15 @@ pub fn step_1(input_content: &str) -> String {
 }
 
 pub fn step_2(input_content: &str) -> String {
-    "0".to_string()
+    let leaderboard = LeaderBoard::from(input_content);
+    leaderboard
+        .get_total_ways_to_win_for_aggregated_races()
+        .to_string()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::step_1;
+    use super::*;
 
     const INPUT_CONTENT: &str = r#"Time:      7  15   30
 Distance:  9  40  200"#;
@@ -81,5 +113,11 @@ Distance:  9  40  200"#;
     fn step_1_should_works() {
         let res = step_1(INPUT_CONTENT);
         assert_eq!(res, "288");
+    }
+
+    #[test]
+    fn step_2_should_works() {
+        let res = step_2(INPUT_CONTENT);
+        assert_eq!(res, "71503");
     }
 }
